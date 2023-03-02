107b138d5bb191eea095e1d3510484d12201e3ce
Ahmadou GUEYE <elahmadou.gueye@univ-thies.sn>
2023-03-02 16:53:40 +0100

    building file change listener

--- PluginDev/src/main/java/com/example/plugindev/ManifestChangeListener.java
+++ PluginDev/src/main/java/com/example/plugindev/ManifestChangeListener.java
@@ -8,6 +8,7 @@ import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.newvfs.BulkFileListener;
+import com.intellij.openapi.vfs.newvfs.events.VFileContentChangeEvent;
 import com.intellij.openapi.vfs.newvfs.events.VFileEvent;
 import com.intellij.openapi.vfs.newvfs.events.VFilePropertyChangeEvent;
 import org.jetbrains.android.dom.AndroidDomElement;
@@ -18,17 +19,24 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class ManifestChangeListener implements BulkFileListener {
+
+    private static final Logger LOGGER = Logger.getLogger(ManifestChangeListener.class.getSimpleName());
 
     @Override
     public void after(@NotNull List<? extends VFileEvent> events) {
         for (VFileEvent event : events) {
             if (Objects.requireNonNull(event.getFile()).getName().equals("AndroidManifest.xml")) {
+                System.out.println("Android manifest file is being updated");
+                LOGGER.log(Level.INFO, "Android manifest file is being updated");
                 if (event instanceof VFilePropertyChangeEvent) {
+                    System.out.println("VFIle propertyChangeEvent detected");
                     // récupération de la propriété modifiée
-                    String propertyName = ((VFilePropertyChangeEvent) event).getPropertyName();
+                    /*String propertyName = ((VFilePropertyChangeEvent) event).getPropertyName();
 
                     // récupération de la valeur de la propriété avant la modification
                     Object oldValue = ((VFilePropertyChangeEvent) event).getOldValue();
@@ -38,7 +46,16 @@ public class ManifestChangeListener implements BulkFileListener {
 
                     // traitement de la modification
                     System.out.println("La propriété " + propertyName + " a été modifiée. Ancienne valeur : "
-                            + oldValue + ". Nouvelle valeur : " + newValue + ".");
+                            + oldValue + ". Nouvelle valeur : " + newValue + ".");*/
+                } else if (event instanceof VFileContentChangeEvent) {
+                    VirtualFile file = event.getFile();
+                    Document document = FileDocumentManager.getInstance().getDocument(file);
+                    assert document != null;
+                    CharSequence before = document.getImmutableCharSequence();
+                    System.out.println("New Content : ");
+                    System.out.println(before);
+                    System.out.println("Old length : " + ((VFileContentChangeEvent) event).getOldLength());
+                    System.out.println("New length : " + ((VFileContentChangeEvent) event).getNewLength());
                 } else {
                     // traitement de l'ajout ou de la suppression de contenu dans le fichier
                     System.out.println("Le fichier AndroidManifest.xml a été modifié.");
