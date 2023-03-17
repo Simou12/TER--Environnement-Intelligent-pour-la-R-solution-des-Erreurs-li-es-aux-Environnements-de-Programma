67a89c44d800bdfe05685fb078d65bcf9f8522b1
Simou12 <117384981+Simou12@users.noreply.github.com>
2023-03-17 22:44:09 +0100

    add file listener

---
+++ PluginDev/src/main/java/add_delete_file/AddFileListener.java
@@ -0,0 +1,35 @@
+package add_delete_file;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.newvfs.BulkFileListener;
+import com.intellij.openapi.vfs.newvfs.events.VFileDeleteEvent;
+import com.intellij.openapi.vfs.newvfs.events.VFileEvent;
+import com.intellij.openapi.vfs.newvfs.events.VFileCreateEvent;
+import org.jetbrains.annotations.NotNull;
+
+
+import java.util.List;
+
+public class AddFileListener implements BulkFileListener {
+
+    private final Project project;
+    public AddFileListener() {
+        this.project = null;
+    }
+
+    public AddFileListener(Project project) {
+        this.project = project;
+    }
+    public void after(@NotNull List<? extends VFileEvent> events){
+            for (VFileEvent event:events){
+                if(event instanceof VFileCreateEvent){
+                    System.out.println("file created path : "+event.getPath());
+
+                }else if(event instanceof VFileDeleteEvent){
+                    System.out.println("file suppressed path : "+event.getPath());
+                }
+            }
+
+    }
+
+}
